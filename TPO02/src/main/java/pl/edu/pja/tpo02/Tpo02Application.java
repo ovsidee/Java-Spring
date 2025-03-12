69eafe39c3819e338fa119689654190cc6ca55a0
Vital <vitalvital1989@gmaIl.com>
2025-03-12 20:18:20 +0100

    Added TPO02 folder

---
+++ TPO02/src/main/java/pl/edu/pja/tpo02/Tpo02Application.java
@@ -0,0 +1,14 @@
+package pl.edu.pja.tpo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpo02Application.class, args);
+
+    }
+
+}
