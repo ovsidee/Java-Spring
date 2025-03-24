a15b3f0205ba592d469a213dab69f77456103b7b
Vital <vitalvital1989@gmaIl.com>
2025-03-24 23:59:31 +0100

    Method getAllEntriesSorted() updated, with LOWER()

--- TPO03/s31719-TPO03-EntityManager/TPO03/src/main/java/pl/edu/pja/tpo03/EntryRepository.java
+++ TPO03/s31719-TPO03-EntityManager/TPO03/src/main/java/pl/edu/pja/tpo03/EntryRepository.java
@@ -36,7 +36,7 @@ public class EntryRepository {
     }
 
     public List<Entry> getAllEntriesSorted(String translationLanguage, String order) {
-        String queryString = "SELECT e FROM Entry e ORDER BY e." + translationLanguage + " " + order;
+        String queryString = "SELECT e FROM Entry e ORDER BY LOWER(e." + translationLanguage + ") " + order;
 
         TypedQuery<Entry> query = entityManager.createQuery(
                 queryString, Entry.class
