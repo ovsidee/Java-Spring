48cafbb2f0d405ec4dfc72ba548375ff495276dd
Vital <vitalvital1989@gmaIl.com>
2025-03-29 17:22:10 +0100

    Added TPO04

---
+++ TPO04/s31719-TPO04-Books/src/main/java/pl/edu/pja/s31719tpo04books/S31719Tpo04BooksApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.s31719tpo04books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S31719Tpo04BooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S31719Tpo04BooksApplication.class, args);
+    }
+
+}
---
+++ TPO04/s31719-TPO04-Books/src/test/java/pl/edu/pja/s31719tpo04books/S31719Tpo04BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.s31719tpo04books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S31719Tpo04BooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
