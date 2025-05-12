b509a8e7d0298010961a7be3add1717a2ad3be2f
Vital <vitalvital1989@gmaIl.com>
2025-05-12 21:23:48 +0200

    added TPO09 removed .zip from TPO08

---
+++ TPO09/TPO09/src/main/java/pl/edu/pja/tpo09/Tpo09Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.tpo09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpo09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpo09Application.class, args);
+    }
+
+}
---
+++ TPO09/TPO09/src/test/java/pl/edu/pja/tpo09/Tpo09ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.tpo09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
