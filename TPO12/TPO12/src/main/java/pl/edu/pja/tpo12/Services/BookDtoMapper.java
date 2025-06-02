f5ec8ffcb62d21c368c146cfb259c2cae3544670
Vital <vitalvital1989@gmaIl.com>
2025-06-02 02:03:49 +0200

    fixed error in BookDTOMapper

--- TPO12/TPO12/src/main/java/pl/edu/pja/tpo12/Services/BookDtoMapper.java
+++ TPO12/TPO12/src/main/java/pl/edu/pja/tpo12/Services/BookDtoMapper.java
@@ -5,6 +5,8 @@ import pl.edu.pja.tpo12.Models.Book;
 import pl.edu.pja.tpo12.Models.DTO.BookDTO;
 import pl.edu.pja.tpo12.Models.Genre;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -12,24 +14,31 @@ public class BookDtoMapper {
     public BookDTO map(Book book) {
         if (book == null)
             return null;
+
         BookDTO dto = new BookDTO();
         dto.setBookID(book.getId());
         dto.setTitle(book.getTitle());
         dto.setIsbn(book.getIsbn());
         dto.setPublicationYear(book.getPublicationYear());
+
         String authors = book.getAuthors()
                 .stream()
                 .map(a -> a.getFirstName() + " " + a.getLastName())
                 .collect(Collectors.joining(", "));
         dto.setAuthor(authors);
+
         if (book.getPublisher() != null) {
-            dto.setPublisher(
-                    String.join(", ",
-                            book.getPublisher().getName(),
-                            book.getPublisher().getAddress(),
-                            book.getPublisher().getCountry())
-            );
+            List<String> publisherParts = new ArrayList<>();
+            if (book.getPublisher().getName() != null)
+                publisherParts.add(book.getPublisher().getName());
+            if (book.getPublisher().getAddress() != null)
+                publisherParts.add(book.getPublisher().getAddress());
+            if (book.getPublisher().getCountry() != null)
+                publisherParts.add(book.getPublisher().getCountry());
+
+            dto.setPublisher(String.join(", ", publisherParts));
         }
+
         dto.setGenres(
                 book.getGenres()
                         .stream()
